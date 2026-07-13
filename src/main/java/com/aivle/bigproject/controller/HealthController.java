254a73282a9aac437189e54e784a37b038227be9
bose9029 <mu9029@naver.com>
2026-07-13 11:05:09 +0900

    feat/Creating Directory and Establishing Settings

---
+++ src/main/java/com/aivle/bigproject/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.aivle.bigproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "BigP-Back Server Running!";
+    }
+}
