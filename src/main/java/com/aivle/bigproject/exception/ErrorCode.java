248c5dd62262d1e7c28ceb4001e519c35a05ec43
HyeonWoo Cho <robert12154937@gmail.com>
2026-07-13 17:47:45 +0900

    중복 폴더 삭제

--- src/main/java/com/aivle/bigproject/exception/ErrorCode.java
+++ src/main/java/com/aivle/bigproject/exception/ErrorCode.java
@@ -8,7 +8,7 @@ public enum ErrorCode {
    
     //에러코드 목록
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
-
+  
 
     private final HttpStatus status;
     private final String message;
