ca405c6d6667c55b4e3bd7881802c427397aab54
Jungyh0 <yh990920@naver.com>
2026-07-14 10:29:17 +0900

    Upload UserRepository

--- src/main/java/com/aivle/bigproject/entity/User.java
+++ src/main/java/com/aivle/bigproject/entity/User.java
@@ -14,28 +14,34 @@ public class User{
 
     @Id
     @GeneratedValue(strategy = GenerationType.PROTECTED)
-    @Column(name = "user_id")
+    @Column(name = "user_id", nullable = false)
     private Integer id;
 
     @Column(nullable = false)
+    @Setter
     private String name;
 
     @Column(name = "company_id")
+    @Setter
     private String companyId;
 
     @Column(name = "login_id", ullable = false, unique = true)
     private String loginId;
 
     @Column(ullable = false)
+    @Setter
     private String password;
 
     @Column(ullable = false)
+    @Setter
     private String role;
 
     @Column(name = "git_id")
+    @Setter
     private String gitId;
 
     @Column(name = "git_name")
+    @Setter
     private String gitName;
 
     @Column(name = "created_at", nullable = false)
---
+++ src/main/java/com/aivle/bigproject/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.java.bigproject.repository;
+
+import com.java.bigproject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public Integer UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByLoginId(String loginId);
+    Optional<User> findByGitId(String gitId);
+    
+    boolean existByLoginId(Strgin loginId);
+    void deleteByLoginId(String loginId);
+}
