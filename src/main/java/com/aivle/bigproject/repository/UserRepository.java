f6057dcca3cba897d053aed772a7ac0c30322012
Jungyh0 <yh990920@naver.com>
2026-07-14 11:09:48 +0900

    upload CompanyRepository

--- src/main/java/com/aivle/bigproject/repository/UserRepository.java
+++ src/main/java/com/aivle/bigproject/repository/UserRepository.java
@@ -4,10 +4,10 @@ import com.java.bigproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public Integer UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByLoginId(String loginId);
     Optional<User> findByGitId(String gitId);
-    
+
     boolean existByLoginId(Strgin loginId);
     void deleteByLoginId(String loginId);
 }
