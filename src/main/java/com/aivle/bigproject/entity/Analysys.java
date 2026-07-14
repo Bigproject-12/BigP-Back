46d9d6d61f0c04c00692f7a725f47d5f86e9a415
Jungyh0 <yh990920@naver.com>
2026-07-14 13:34:24 +0900

    Upload Analysis Entity Class

---
+++ src/main/java/com/aivle/bigproject/entity/Analysys.java
@@ -0,0 +1,37 @@
+package com.aivle.bigproject.entity;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@Setter
+@Table(name = "ANALYSIS")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Analysis {
+
+    @Id
+    @Column(name = "analysis_id", nullalbe = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "repo_id", nullable = false)
+    private Integer repoId;
+
+    @Column(name = "company_id", nullable = false)
+    private Integer companyId;
+
+    @Column(name = "origin_code", nullalbe = false, columnDefinition = "TEXT")
+    private String originCode;
+
+    @Column(nullalbe = false)
+    private String languege;
+
+    @Column(nullable = false)
+    private String prompt;
+
+    @Column(nullable = false)
+    private String status;
+}
