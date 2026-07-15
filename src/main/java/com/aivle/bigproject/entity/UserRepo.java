e32af7f94595977a4ae3bc8f8e1faa593c9120ff
Jungyh0 <yh990920@naver.com>
2026-07-15 10:58:34 +0900

    refactor: add import for date

--- src/main/java/com/aivle/bigproject/entity/UserRepo.java
+++ src/main/java/com/aivle/bigproject/entity/UserRepo.java
@@ -3,6 +3,8 @@ package com.aivle.bigproject.entity;
 import lombok.*;
 import jakarta.persistence.*;
 import java.time.LocalDate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 @Builder
