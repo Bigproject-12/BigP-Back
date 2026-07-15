19516da2e132a895ce4a686c76d91d55616de226
Jungyh0 <yh990920@naver.com>
2026-07-15 14:23:57 +0900

    refactor: fix typos

--- src/main/java/com/aivle/bigproject/repository/CompanyRepository.java
+++ src/main/java/com/aivle/bigproject/repository/CompanyRepository.java
@@ -4,5 +4,5 @@ import com.aivle.bigproject.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
-    boolean existsByCompanyId(Integer id);
+    boolean existsById(Integer id);
 }
