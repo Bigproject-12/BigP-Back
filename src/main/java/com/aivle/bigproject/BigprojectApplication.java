835b329ce148bc2c0a032bd172df318a4b275fa0
Jungyh0 <yh990920@naver.com>
2026-07-20 11:17:23 +0900

    Upload Github Connecting DTO

--- src/main/java/com/aivle/bigproject/BigprojectApplication.java
+++ src/main/java/com/aivle/bigproject/BigprojectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class BigprojectApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BigprojectApplication.class, args);
 	}
