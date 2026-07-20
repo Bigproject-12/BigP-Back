4a229e6fa005a09eb6bf313058d6cfa6654fd7a7
Jungyh0 <yh990920@naver.com>
2026-07-20 14:57:47 +0900

    refacor: Update User Service method for Github Token Filed

--- src/main/java/com/aivle/bigproject/controller/GithubController.java
+++ src/main/java/com/aivle/bigproject/controller/GithubController.java
@@ -1,8 +1,10 @@
 package com.aivle.bigproject.controller;
 
-import com.aivle.bigproject.dto.user.GithubConnect; 
+import com.aivle.bigproject.dto.user.GithubConnect;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import com.aivle.bigproject.service.UserService;
 
@@ -17,11 +19,14 @@ public class GithubController {
     }
 
     @PostMapping
-    public ResponseEntity<?> connectRepository(@Valid @RequestBody GithubConnect request) {
+    public ResponseEntity<?> connectRepository(@Valid @RequestBody GithubConnect request, Authentication authentication) {
         try {
             String token = request.githubToken(); 
             
             System.out.println("프론트에서 넘어온 깃허브 토큰: " + token);
+
+            Integer userId = Integer.parseInt(authentication.getName());
+            userService.saveGithubToken(userId, request.githubToken());
             
             return ResponseEntity.ok().body("레포지토리 연동이 성공적으로 완료되었습니다.");
 
@@ -29,6 +34,7 @@ public class GithubController {
             return ResponseEntity.badRequest().body(e.getMessage());
 
         } catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.internalServerError().body("서버 오류가 발생했습니다.");
         }
     } 
