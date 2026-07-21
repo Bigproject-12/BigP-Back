6e9f49f700d381557aec1af043b8527f29fe2640
HyeonWoo Cho <robert12154937@gmail.com>
2026-07-21 17:10:04 +0900

    feat: Favorite Repositories function added, ErrorCode edited

--- src/main/java/com/aivle/bigproject/repository/RepoFavRepository.java
+++ src/main/java/com/aivle/bigproject/repository/RepoFavRepository.java
@@ -1,7 +1,13 @@
 package com.aivle.bigproject.repository;
 
 import com.aivle.bigproject.entity.RepoFav;
+import com.aivle.bigproject.entity.User;
+import com.aivle.bigproject.entity.GithubRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface RepoFavRepository extends JpaRepository<RepoFav, Integer> {
+    boolean existsByUserAndGithubRepo(User user, GithubRepo githubRepo);
+    List<RepoFav> findAllByUser_Id(Integer userId);
+    void deleteByUserAndGithubRepo(User user, GithubRepo githubRepo);
 }
