3fc3fac0bad5e87d96d088289ce483d1224dfb5a
minwook-Kang <yjo2567@gmail.com>
2026-07-21 15:26:12 +0900

    feat: admin memberDetailPage

--- src/main/java/com/aivle/bigproject/controller/UserController.java
+++ src/main/java/com/aivle/bigproject/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -87,5 +88,12 @@ public class UserController {
             @PageableDefault(size = 20, sort = "id") Pageable pageable
     ) {
         return userService.findAll(pageable);
-    }   
+    }
+
+    /** 관리자가 회원 목록에서 특정 회원을 선택했을 때 상세 정보를 조회한다. */
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public UserResponse findById(@PathVariable Integer id) {
+        return userService.getUser(id);
+    }
 }
