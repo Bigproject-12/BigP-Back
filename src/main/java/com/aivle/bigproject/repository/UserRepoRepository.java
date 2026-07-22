f990d8902e0f9837aad382c4f68155940ded8ea8
bose9029 <mu9029@naver.com>
2026-07-22 14:25:56 +0900

    feat/Add a feature to view user's repositories

--- src/main/java/com/aivle/bigproject/repository/UserRepoRepository.java
+++ src/main/java/com/aivle/bigproject/repository/UserRepoRepository.java
@@ -8,11 +8,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepoRepository extends JpaRepository<UserRepo, Integer> {
     boolean existsByUserAndGithubRepo(User user, GithubRepo githubRepo);
 
     List<UserRepo> findAllByUserId(Integer userId);
+
+    Optional<UserRepo> findByUser_IdAndGithubRepo_Id(Integer userId, Integer repoId);
 
     @Modifying
     @Query(value = "DELETE FROM USER_REPO WHERE user_id = :userId", nativeQuery = true)
