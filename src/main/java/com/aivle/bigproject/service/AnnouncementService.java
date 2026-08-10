e62e753e7e7fbe26589553d351aa49ebe40d1b9e
minwook-Kang <yjo2567@gmail.com>
2026-08-10 09:54:06 +0900

    docs:importdel

--- src/main/java/com/aivle/bigproject/service/AnnouncementService.java
+++ src/main/java/com/aivle/bigproject/service/AnnouncementService.java
@@ -17,21 +17,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import java.nio.charset.StandardCharsets;
-import org.springframework.web.util.UriUtils;
+
 
 @Service
 @RequiredArgsConstructor
